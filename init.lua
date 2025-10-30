60b014215a2645213bdabf9702f06169fab66006
Tawxyn <oleshchukmike@gmail.com>
2025-10-30 11:38:55 -0700

    Added pairs for mini nvim

--- init.lua
+++ init.lua
@@ -402,6 +402,7 @@ require("lazy").setup({
 		config = function()
 			require("mini.ai").setup({ n_lines = 500 })
 			require("mini.surround").setup()
+			require("mini.pairs").setup()
 		end,
 	},
 
